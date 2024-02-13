0e9b6e415149b0542d8d85240f62e7cb5817132d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-13 03:25:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// aos
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return <div className="bg-primary">App</div>;
 };
 
