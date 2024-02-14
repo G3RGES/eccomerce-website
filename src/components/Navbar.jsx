3655a267e82c5f1415308a4387f324d6f5039675
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-14 14:07:41 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,18 +60,25 @@ const DropdownLinks = [
 
 const Navbar = () => {
   return (
-    <div className="">
+    <div
+      className="shadow-md bg-white dark:bg-gray-900 dark:text-white 
+    duration-200 relative z-40 "
+    >
       {/* upper navbar */}
-      <div className="">
-        <div className="">
+      <div className="bg-primary/40 py-2">
+        <div className="container flex justify-between items-center ">
           <div className="">
-            <a className="" href="#">
-              <img className="" src={Logo} alt="logo" />
+            <a
+              className="uppercase font-bold text-2xl sm:text-3xl flex gap-2"
+              href="#"
+            >
+              <img className="w-10" src={Logo} alt="logo" />
+              Shopsy
             </a>
           </div>
           {/* search bar and order button */}
           <div className="">
-            <div className="group">
+            <div className="relative group hidden sm:block">
               <input
                 type="text"
                 placeholder="Search..."
@@ -80,8 +87,27 @@ const Navbar = () => {
                  px-2 py-1 focus:outline-none focus:border-1 focus:border-primary
                dark:border-gray-500 dark:bg-gray-800  "
               />
+              <IoMdSearch
+                className="text-gray-500 group-hover:text-primary 
+              absolute top-1/2 -translate-y-1/2 right-3"
+              />
             </div>
           </div>
+          {/* order button */}
+          <button
+            className="bg-gradient-to-r from-primary to-secondary
+             transition-all duration-200 text-white  py-1 px-4 rounded-full
+              flex items-center gap-3 group"
+            onClick={() => alert("order not available yet")}
+          >
+            <span className="group-hover:block hidden transition-all duration-200 ">
+              Order
+            </span>
+            <FaCartShopping
+              className="text-xl text-white drop-shadow-sm
+             cursor-pointer"
+            />
+          </button>
         </div>
       </div>
       {/* lower navbar */}
