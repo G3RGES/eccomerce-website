dce1293a7d3f9ed41ee767ecb314e397da052361
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-14 13:37:05 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -69,6 +69,19 @@ const Navbar = () => {
               <img className="" src={Logo} alt="logo" />
             </a>
           </div>
+          {/* search bar and order button */}
+          <div className="">
+            <div className="">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px]
+                 transition-all duration-300 rounded-full border border-gray-300
+                 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary
+               dark:border-gray-500 dark:bg-gray-800  "
+              />
+            </div>
+          </div>
         </div>
       </div>
       {/* lower navbar */}
