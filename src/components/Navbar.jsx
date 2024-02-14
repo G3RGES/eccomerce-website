58e1548e3c0367593a6edd15eb352deb54c5b6f7
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-14 13:20:45 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,28 @@
 /*  eslint-disable no-unused-vars */
 import React from "react";
 
+// icons
+import { IoMdSearch } from "react-icons/io";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown } from "react-icons/fa";
+
+// assets
+import Logo from "../assets/logo.png";
+
+// components
+// import DarkMode from "./DarkMode";
+
 const Navbar = () => {
   return (
     <div className="">
       {/* upper navbar */}
-      <div className=""></div>
+      <div className="">
+        <div className="">
+          <div className="">
+            <a className="" href="#"></a>
+          </div>
+        </div>
+      </div>
       {/* lower navbar */}
       <div className=""></div>
     </div>
