35e2337b25d7d7691d6d8335468e548de900c758
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-15 05:14:33 +0200

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="">Hero</div>
+    </div>
+  );
+};
+
+export default Hero;
