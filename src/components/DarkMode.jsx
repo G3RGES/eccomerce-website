f4a09ddebb9b0458d4387f10bfac24ccc4c9dac4
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-15 03:48:31 +0200

    Update DarkMode.jsx

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,8 +1,35 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
+
+// assets
+import LightButton from "../assets/website/light-mode-button.png";
+import DarkButton from "../assets/website/dark-mode-button.png";
 
 const DarkMode = () => {
-  return <div>DarkMode</div>;
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  return (
+    <div className="relative">
+      {/* light mode */}
+      <img
+        className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all
+        duration-300 absolute right-0 z-10
+        `}
+        src={LightButton}
+        alt="light mode"
+      />
+
+      {/* dark mode */}
+      <img
+        className={`w-12 cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all
+        duration-300 `}
+        src={DarkButton}
+        alt="dark mode"
+      />
+    </div>
+  );
 };
 
 export default DarkMode;
