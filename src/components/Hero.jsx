b88f8d16bf79b05fd380b01a7875cce6b40af7e1
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-16 15:10:03 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,71 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// assets
+import Image1 from "../assets/hero/women.png";
+import Image2 from "../assets/hero/shopping.png";
+import Image3 from "../assets/hero/sale.png";
+
+const imageList = [
+  {
+    id: 1,
+    img: Image2,
+    title: "Upto 50% off on all Men's wear",
+    discription: "biggest sale ever on Men's wear",
+  },
+  {
+    id: 2,
+    img: Image1,
+    title: "Upto 30% off on all Women's wear",
+    discription: "biggest sale ever on Women's wear",
+  },
+  {
+    id: 3,
+    img: Image3,
+    title: "Upto 70% off on all Products",
+    discription: "biggest sale ever on all Products",
+  },
+];
+
 const Hero = () => {
   return (
-    <div>
-      <div className="">Hero</div>
+    <div
+      className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100
+    flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200  "
+    >
+      {/* background pattern */}
+      <div
+        className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 
+      rounded-3xl rotate-45 -z-9 dark:bg-tertiary/40  "
+      ></div>
+
+      {/* hero section */}
+      <div className="container pb-8 sm:pb-0 ">
+        <div className="">
+          <div className="grid grid-cols-1 sm:grid-cols-2 ">
+            {/* text content section */}
+            <div className="">
+              <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">
+                Lorem ipsum dolor sit
+              </h1>
+              <p className="text-sm">
+                lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae,
+                ad
+              </p>
+              <div className="">
+                <button
+                  className="bg-gradient-to-r from-primary to-secondary 
+                hover:scale-105 duration-200 text-white py-2 px-4 rounded-full"
+                >
+                  Order now
+                </button>
+              </div>
+            </div>
+
+            {/* image section */}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
