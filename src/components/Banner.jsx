16c5546c856897e625d7d547c18f18ab9f8225d0
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-17 09:07:48 +0200

    Create Banner.jsx

---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+// icons
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io5";
+import { GiFoodTruck } from "react-icons/gi";
+
+// assets
+import BannerImg from "../assets/women/women2.jpg";
+
+const Banner = () => {
+  return (
+    <div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Banner;
