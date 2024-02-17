6757e7173dbcdf51cb2d27f2bbd2a02e2320a0aa
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-17 07:52:43 +0200

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// icons
+import { FaStar } from "react-icons/fa6";
+
+// assets
+import Img1 from "../assets/women/women.png";
+import Img2 from "../assets/women/women2.jpg";
+import Img3 from "../assets/women/women3.jpg";
+import Img4 from "../assets/women/women4.jpg";
+
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div>
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default Products;
