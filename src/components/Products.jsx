877901cc752cce5ce636ef5706c748529ce7fc7c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-17 08:11:32 +0200

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -10,10 +10,92 @@ import Img2 from "../assets/women/women2.jpg";
 import Img3 from "../assets/women/women3.jpg";
 import Img4 from "../assets/women/women4.jpg";
 
+const ProductsData = [
+  {
+    id: 1,
+    img: Img1,
+    title: "Women Ethnic",
+    rating: 5.0,
+    color: "white",
+    aosDelay: "0",
+  },
+  {
+    id: 2,
+    img: Img2,
+    title: "Women western",
+    rating: 4.5,
+    color: "Red",
+    aosDelay: "200",
+  },
+  {
+    id: 3,
+    img: Img3,
+    title: "Goggles",
+    rating: 4.7,
+    color: "brown",
+    aosDelay: "400",
+  },
+  {
+    id: 4,
+    img: Img4,
+    title: "Printed T-Shirt",
+    rating: 4.4,
+    color: "Yellow",
+    aosDelay: "600",
+  },
+  {
+    id: 5,
+    img: Img2,
+    title: "Fashin T-Shirt",
+    rating: 4.5,
+    color: "Pink",
+    aosDelay: "800",
+  },
+];
+
 const Products = () => {
   return (
-    <div>
-      <div className=""></div>
+    <div className="mt-14 mb-12">
+      <div className="container">
+        {/* header section */}
+        <div className="text-center mb-10 max-w-[600px] mx-auto ">
+          <p className="text-sm text-primary dark:text-tertiary ">
+            Top Selling Products for you
+          </p>
+          <h1 className="text-3xl font-bold">Products</h1>
+          <p className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+            asperiores modi Sit asperiores modi
+          </p>
+        </div>
+
+        {/* body section */}
+        <div>
+          <div
+            className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 
+          lg:grid-cols-5 place-items-center gap-5"
+          >
+            {/* card section */}
+            {ProductsData.map((product) => (
+              <div className="space-y-3" key={product.id}>
+                <img
+                  className="h-[220px] w-[150px] object-cover rounded-md  "
+                  src={product.img}
+                  alt={product.title}
+                />
+                <div>
+                  <h3 className="font-semibold">{product.title}</h3>
+                  <p className="text-sm text-gray-600">{product.color} </p>
+                  <div className="flex flex-row items-center gap-2">
+                    <FaStar className="text-yellow-500" />
+                    <span>{product.rating}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
