787bd4c27ca35c254762d5054d03dbacb9ba9b86
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-17 07:52:40 +0200

    Create TopProducts.jsx

---
+++ src/components/TopProducts.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+// icons
+import { FaStar } from "react-icons/fa";
+
+// assets
+import Img1 from "../assets/shirt/shirt.png";
+import Img2 from "../assets/shirt/shirt2.png";
+import Img3 from "../assets/shirt/shirt3.png";
+
+const TopProducts = () => {
+  return (
+    <div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default TopProducts;
