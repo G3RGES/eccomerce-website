73393407912678089258550828c513da7bd941ab
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-18 13:30:44 +0200

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -69,8 +69,27 @@ const Testimonials = () => {
   };
 
   return (
-    <div>
-      <div className=""></div>
+    <div className="py-10">
+      <div className="container">
+        {/* header section */}
+        <div className="text-center mb-10 max-w-[600px] mx-auto ">
+          <p
+            data-aos="fade-up"
+            className="text-sm text-primary dark:text-tertiary "
+          >
+            What our customers are saying
+          </p>
+          <h1 data-aos="zoom-out" className="text-3xl font-bold">
+            Testimonials
+          </h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+            asperiores modi Sit asperiores modi
+          </p>
+        </div>
+
+        {/* Testimonial cards */}
+      </div>
     </div>
   );
 };
