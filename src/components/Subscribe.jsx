dda9e6f83b33c2f12dd904972070c91c0e270a71
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-18 13:26:27 +0200

    Update Subscribe.jsx

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -21,15 +21,15 @@ const Subscribe = () => {
       style={BannerImg}
     >
       <div className="container backdrop-blur-sm py-10">
-        <div className="">
-          <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold">
+        <div className="space-y-6 max-w-xl mx-auto">
+          <h1 className="text-2xl !text-center sm:text-left sm:text-4xl font-semibold">
             Get Notified About New Products
           </h1>
           <input
             data-aos="fade-up"
             type="text"
             placeholder="Enter your email"
-            className="w-full p-3"
+            className="w-full p-3 rounded-xl"
           />
         </div>
       </div>
