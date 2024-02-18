e7949fe9f6950f08d84c9f2f744349a1acee6d44
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-18 13:52:24 +0200

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -69,7 +69,7 @@ const Testimonials = () => {
   };
 
   return (
-    <div className="py-10">
+    <div className="py-10 mb-10">
       <div className="container">
         {/* header section */}
         <div className="text-center mb-10 max-w-[600px] mx-auto ">
@@ -89,6 +89,37 @@ const Testimonials = () => {
         </div>
 
         {/* Testimonial cards */}
+        <div data-aos="zoom-in">
+          <Slider {...settings}>
+            {TestimonialData.map((data) => (
+              <div className="my-6" key={data.id}>
+                <div
+                  className="flex flex-col gap-4 shadow-lg py-8 px-6 mx-4 
+                rounded-xl dark:bg-gray-800 bg-primary/10 relative"
+                >
+                  <div className="mb-4">
+                    <img
+                      src={data.img}
+                      alt=""
+                      className="rounded-full w-20 h-20"
+                    />
+                  </div>
+                  <div className="flex flex-col items-center gap-4">
+                    <div className="space-y-3">
+                      <p className="text-xs text-gray-500">{data.text}</p>
+                      <h1 className="text-xl font-bold text-black/80 dark:text-light">
+                        {data.name}
+                      </h1>
+                    </div>
+                  </div>
+                  <p className="text-black/20 text-9xl font-serif absolute top-0 right-0">
+                    ,,
+                  </p>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
