5fa25956a7b84fe55aa23802f1dccf660848c33d
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-18 13:55:24 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,47 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// assets
+import footerLogo from "../assets/logo.png";
+import Banner from "../assets/website/footer-pattern.jpg";
+
+// icons
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+
+const BannerImg = {
+  backgroundImage: `url(${Banner})`,
+  backgroundPosition: "bottom",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100%",
+  width: "100%",
+};
+
+const FooterLinks = [
+  {
+    title: "Home",
+    link: "/#",
+  },
+  {
+    title: "About",
+    link: "/#about",
+  },
+  {
+    title: "Contact",
+    link: "/#contact",
+  },
+  {
+    title: "Blog",
+    link: "/#blog",
+  },
+];
+
 const Footer = () => {
   return (
     <div className="">
