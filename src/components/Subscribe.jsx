24aa64f6d28170de5a2cbed94ee45e8512c32424
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-18 12:55:06 +0200

    Create Subscribe.jsx

---
+++ src/components/Subscribe.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const Subscribe = () => {
+  return (
+    <div>
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Subscribe;
