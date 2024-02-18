4ccc01260672837206f66510a4c648208c681a30
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-18 13:26:24 +0200

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -104,6 +104,15 @@ const Products = () => {
               </div>
             ))}
           </div>
+          {/* view all button */}
+          <div className="flex justify-center">
+            <button
+              className="text-center mt-10 cursor-pointer bg-primary
+             text-white dark:bg-tertiary py-1 px-5 rounded-md"
+            >
+              View All Button
+            </button>
+          </div>
         </div>
       </div>
     </div>
