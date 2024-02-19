ea5b1a2c74307caa922646d845f037224eec04bd
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-19 07:30:59 +0200

    Update Popup.jsx

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// icons
+import { IoCloseOutline } from "react-icons/io5";
+
 const Popup = () => {
   return <div>Popup</div>;
 };
