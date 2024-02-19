25a890bf11381250595467044018034c8c5a8f5c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-19 07:47:03 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,8 +44,31 @@ const FooterLinks = [
 
 const Footer = () => {
   return (
-    <div className="">
-      <div className=""></div>
+    <div style={BannerImg} className="text-white mb-20">
+      <div className="container">
+        <div className="grid md:grid-cols-3 pb-44 pt-5">
+          {/* company details */}
+          <div className="py-8 px-4">
+            <h1
+              className="sm:text-3xl text-xl font-bold sm:text-left 
+            text-justify mb-3 flex items-center gap-3"
+            >
+              <img
+                className="max-w-[50px] "
+                src={footerLogo}
+                alt="footer_logo"
+              />
+              Shopsy
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum in
+              beatae ea recusandae blanditiis veritatis.
+            </p>
+          </div>
+
+          {/* footer links */}
+        </div>
+      </div>
     </div>
   );
 };
