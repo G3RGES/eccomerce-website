064a386bae83c247d312019d2a2c79bc3744a078
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-19 08:33:05 +0200

    Update Popup.jsx

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,11 +1,20 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
 
 // icons
 import { IoCloseOutline } from "react-icons/io5";
 
-const Popup = () => {
-  return <div>Popup</div>;
+const Popup = ({ orderPopup, setOrderPopup }) => {
+  return (
+    <>
+      {orderPopup && (
+        <div className="">
+          <div className=""></div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Popup;
