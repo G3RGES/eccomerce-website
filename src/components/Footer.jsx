dbf2054f739e44a1a84cf518ecba09d64b3257e5
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-19 08:22:10 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,20 +44,13 @@ const FooterLinks = [
 
 const Footer = () => {
   return (
-    <div style={BannerImg} className="text-white mb-20">
+    <div style={BannerImg} className="text-white">
       <div className="container">
-        <div className="grid md:grid-cols-3 pb-44 pt-5">
+        <div data-aos="zoom-in" className="grid md:grid-cols-3 pb-44 pt-5">
           {/* company details */}
           <div className="py-8 px-4">
-            <h1
-              className="sm:text-3xl text-xl font-bold sm:text-left 
-            text-justify mb-3 flex items-center gap-3"
-            >
-              <img
-                className="max-w-[50px] "
-                src={footerLogo}
-                alt="footer_logo"
-              />
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
+              <img src={footerLogo} alt="" className="max-w-[50px]" />
               Shopsy
             </h1>
             <p>
@@ -66,18 +59,66 @@ const Footer = () => {
             </p>
           </div>
 
-          {/* footer links */}
-          <div className="grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+          {/* Footer Links */}
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
             <div>
-              <div className="py-8 px-4 ">
+              <div className="py-8 px-4">
                 <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
                   Important Links
                 </h1>
-                <ul className="">
-                  {FooterLinks.map((link, idx) => (
-                    <li className="" key={idx}></li>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200"
+                      key={link.title}
+                    >
+                      <a href={link.link}>{link.title}</a>
+                    </li>
                   ))}
                 </ul>
+              </div>
+            </div>
+            <div>
+              <div className="py-8 px-4">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  {FooterLinks.map((link) => (
+                    <li
+                      className="cursor-pointer hover:text-primary hover:translate-x-1 duration-300 text-gray-200"
+                      key={link.title}
+                    >
+                      <a href={link.link}>{link.title}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            {/* social links */}
+
+            <div>
+              <div className="flex items-center gap-3 mt-6">
+                <a href="https://www.instagram.com/gergesnashaat/">
+                  <FaInstagram className="text-3xl" />
+                </a>
+                <a href="https://www.facebook.com/gergesnashaat95">
+                  <FaFacebook className="text-3xl" />
+                </a>
+                <a href="https://www.linkedin.com/in/gergesnashaat/">
+                  <FaLinkedin className="text-3xl" />
+                </a>
+              </div>
+              <div className="mt-6">
+                <div className="flex items-center gap-3">
+                  <FaLocationArrow />
+                  <p>Sohag, Egypt</p>
+                </div>
+                <div className="flex items-center gap-3 mt-3">
+                  <FaMobileAlt />
+                  <p>+20 1203346582</p>
+                </div>
               </div>
             </div>
           </div>
