37af1bd870f7049482220a2048f317c7a467560a
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-19 09:13:16 +0200

    Update TopProducts.jsx

--- src/components/TopProducts.jsx
+++ src/components/TopProducts.jsx
@@ -95,7 +95,7 @@ const TopProducts = ({ handleOrderPopup }) => {
                 </p>
                 <button
                   className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary"
-                  //   onClick={handleOrderPopup}
+                  onClick={handleOrderPopup}
                 >
                   Order Now
                 </button>
