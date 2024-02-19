620b8c683835bbc8b5b8869522e946e8251c2680
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-19 08:50:17 +0200

    Update Popup.jsx

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -13,7 +13,15 @@ const Popup = ({ orderPopup, setOrderPopup }) => {
           <div
             className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 
           backdrop-blur-md "
-          ></div>
+          >
+            <div
+              className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white 
+              dark:bg-gray-900 p-4 shadow-md rounded-md dark:text-white"
+            >
+              {/* header */}
+              {/* body */}
+            </div>
+          </div>
         </div>
       )}
     </>
