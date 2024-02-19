2c5c242ee717f7e9ce7bf60adaa565ef22408f37
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-19 08:29:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="">
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar />
       <Hero />
       <Products />
